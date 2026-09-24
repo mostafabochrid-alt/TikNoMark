c0c02367f419fac7d215bf47cd379cf7c979ab91
mostafabochrid-alt <mostafabochrid@gmail.com>
2026-09-24 21:22:04 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'savetikx-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+
